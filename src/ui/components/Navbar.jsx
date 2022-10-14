cdbacc9bf512e2d4a0c4c24477280ee936137666
Herbert Gutierrez <dev.gutti1391@gmail.com>
2022-10-13 18:19:40 -0600

    NavBar mode

--- src/ui/components/Navbar.jsx
+++ src/ui/components/Navbar.jsx
@@ -47,22 +47,7 @@ export const Navbar = () => {
                 </div>
             </div>
 
-            <div className="navbar-collapse collapse w-100 order-3 dual-collapse2 d-flex justify-content-end">
-                <ul className="navbar-nav ml-auto">
-                   
-                    <span className="nav-item nav-link text-primary">
-                        Fernando
-                    </span>
-
-                    <button
-                        className="nav-item nav-link btn"
-                        onClick={ onLogout }
-                    >
-                        Logout
-                    </button>
-
-                </ul>
-            </div>
+            
         </nav>
     )
 }
